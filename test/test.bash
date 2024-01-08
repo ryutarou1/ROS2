f6270d404b54fbe68cb041b4b5aef8f54e7ceb35
Ryutaro Kanno <rkanno1014@yahoo.co.jp>
2024-01-08 09:38:25 +0900

    8

---
+++ test/test.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: 2023 kanno ryutaro
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+dir=~
+[ "$1" != "" ] && dir="$1"
+
+cd $dir/ros2_ws
+colcon build
+source $dir/.bashrc
+timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
+
+cat /tmp/mypkg.log |
+grep 'process has finished cleanly'
