43bae3a593cf5fa28b01435151aff1d0bca1e44e
Ryutaro Kanno <rkanno1014@yahoo.co.jp>
2024-01-14 11:09:24 +0900

    7

--- test/test.bash
+++ test/test.bash
@@ -9,9 +9,6 @@ cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
 
-# person_msgs パッケージ内の Query サービスのインターフェースをログに追加
-ros2 interface show "person_msgs/srv/Query" >> /tmp/mypkg.log
-
 timeout 10 ros2 launch mypkg talk_listen.launch.py >> /tmp/mypkg.log
 
 # /tmp/mypkg.log ファイルから ': 21' を含む行だけを表示
