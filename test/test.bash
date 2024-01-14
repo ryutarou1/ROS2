a15227e91fcc5986fa3c868497391b196f25d940
Ryutaro Kanno <rkanno1014@yahoo.co.jp>
2024-01-14 10:37:30 +0900

    7

--- test/test.bash
+++ test/test.bash
@@ -8,7 +8,7 @@ dir=~
 cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
-ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
+timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 
 cat /tmp/mypkg.log |
-grep ' [listener]: age'
+grep ': 21'
