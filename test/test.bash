58b86f38002689b02d53786998c3aca1aaa93f98
ryutarou1 <147678431+ryutarou1@users.noreply.github.com>
2024-10-03 12:14:58 +0900

    Update test.bash

--- test/test.bash
+++ test/test.bash
@@ -1,16 +1,40 @@
-#!/bin/bash
-# SPDX-FileCopyrightText: 2023 kanno ryutaro
-# SPDX-License-Identifier: BSD-3-Clause
+#!/bin/bash -xv
+# SPDX-FileCopyrightText: 2023 Toshito Kimura
+# SPDX-License-Identifire: BSD-3-Clause
+
 
 dir=~
 [ "$1" != "" ] && dir="$1"
+
+source $dir/.bashrc
+
+cd $dir/ros2_ws/src
+
+git clone https://github.com/oO0x4x0Oo/person_msgs.git
 
 cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
 
-timeout 10 ros2 launch mypkg talk_listen.launch.py >> /tmp/mypkg.log
+ros2 interface show "person_msgs/srv/Query"
 
-# /tmp/mypkg.log ファイルから ': 21' を含む行だけを表示
-cat /tmp/mypkg.log | grep ': 21'
+cd $dir/ros2_ws
+colcon build
+source $dir/.bashrc
 
+timeout 2 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
+
+ng () {
+    echo "NG at Line $1"
+    res=1
+}
+
+res=0
+
+cat /tmp/mypkg.log |
+grep ": 20"
+
+[ "$?" = 0 ] || ng ${LINENO}
+
+[ "$res" = 0 ] && echo "OK"
+exit $res
